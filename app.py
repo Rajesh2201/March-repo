474c3e75fc437afc6f749d6ef65d608523f58efb
Rajesh Teki <raj.storage23@gmail.com>
2026-04-06 23:51:50 +0530

    added code

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 python is lrning
+Open an SSH client.
+
+Locate your private key file. The key used to launch this instance is march-kp.pem
+
+Run this command, if necessary, to ensure your key is not publicly viewable.
+chmod 400 "march-kp.pem"
+
+Connect to your instance using its Public DNS:
+ec2-13-40-51-125.eu-west-2.compute.amazonaws.com
